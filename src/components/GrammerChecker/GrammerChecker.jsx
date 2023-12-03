02be571ee782fd798e53674c7ad256f1b90e3153
Ian Ross <ianrossdrums@gmail.com>
2023-12-03 12:54:03 -0700

    Fix error message

--- src/components/GrammerChecker/GrammerChecker.jsx
+++ src/components/GrammerChecker/GrammerChecker.jsx
@@ -1,20 +1,19 @@
 import React, { useState } from 'react';
 
-export const Thesaurus = () => {
-    const [word, setWord] = useState('');
+export const GrammerChecker = () => {
+    const [message, setMessage] = useState('');
     const [response, setResponse] = useState('');
     const [isPending, setIsPending] = useState(false);
   
     const handleSubmit = (e) => {
       e.preventDefault();
       setIsPending(true);
-
-      fetch('https://poem-assistant-api.onrender.com/thesaurus', {
+      fetch('https://poem-assistant-api.onrender.com/grammer-checker', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ word: word }),
+        body: JSON.stringify({ message: message }),
       })
         .then((res) => res.json())
         .then((data) => {
@@ -25,15 +24,18 @@ export const Thesaurus = () => {
     };
   
     return (
-      <div className="form">
-        <h3>Thesaurus</h3>
-        <form className="poem-form" onSubmit={handleSubmit}>
-          <label htmlFor="quantity">What word would you like to check:</label>
-          <input type="text" id="word" name="word" onChange={(e) => setWord(e.target.value)}/>
-          <br /><br />
-          { !isPending && <button type="submit">Submit</button>}
-          { isPending && <button type="submit" disabled>Finding words...</button>}
-        </form>
+        <div className="form">
+          <h3>Grammer Checker</h3>
+          <form onSubmit={handleSubmit}>
+            <textarea
+              value={message}
+              placeholder="Add text here"
+              onChange={(e) => setMessage(e.target.value)}
+            ></textarea>
+            { !isPending && <button type="submit">Submit</button>}
+            { isPending && <button type="submit" disabled>Checking Grammer...</button>}
+          <br />
+          </form>
         <div id="response">{response}</div>
       </div>
     );
