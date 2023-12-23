dbbc315c8ff64fbb273faa64e3ef291f5a3d8f59
Ian Ross <ianrossdrums@gmail.com>
2023-12-23 09:58:33 -0700

    Refactor https requests to use axios

--- src/components/PromptGenerator/PromptGenerator.jsx
+++ src/components/PromptGenerator/PromptGenerator.jsx
@@ -1,46 +1,57 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 
 export const PromptGenerator = () => {
-    const [amount, setAmount] = useState(1.0);
-    const [response, setResponse] = useState('');
-    const [isPending, setIsPending] = useState(false);
-  
-    const handleSubmit = (e) => {
-      e.preventDefault();
-      setIsPending(true);
+  const [amount, setAmount] = useState(1.0);
+  const [response, setResponse] = useState('');
+  const [isPending, setIsPending] = useState(false);
+  const [error, setError] = useState('');
 
-      fetch('https://poem-assistant-api.onrender.com/prompt-generator', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ amount: amount }),
-      })
-        .then((res) => res.json())
-        .then((data) => {
-          setResponse(data.message);
-          setIsPending(false);
-        })
-        .catch((error) => console.error("there is an error", error));
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setIsPending(true);
+    setError('');
+
+    const headers = {
+      'Content-Type': 'application/json'   
     };
-  
-    return (
-      <div className="component-container">
-        <div className="form">
-          <h3>Prompt Generator</h3>
-          <form className="poem-form" onSubmit={handleSubmit}>
-            <label htmlFor="quantity">How many poem prompts do you want:</label>
-            <input type="number" id="quantity" name="quantity" min="1" onChange={(e) => setAmount(e.target.value)} />
-            <br /><br />
-            { !isPending && <button type="submit" className="submit">Submit</button>}
-            { isPending && <button type="submit" className="submit" disabled>Generating Prompts...</button>}
-          </form>
-          { response && (
-            <div className="response-container">
-              <div className="response">{response}</div>
-            </div>
-          )}
-        </div>
-      </div>
-    );
+    const requestBody = {
+      amount: amount
+    };
+
+    await axios.post('https://poem-assistant-api.onrender.com/prompt-generator', requestBody, { headers })
+    .then(response => {
+      setResponse(response.data.message);
+      setIsPending(false);
+    })
+    .catch(error => {
+      setError(error.message);
+      setIsPending(false);
+    });
   };
+
+  return (
+    <div className="component-container">
+      <div className="form">
+        <h3>Prompt Generator</h3>
+        <form className="poem-form" onSubmit={handleSubmit}>
+          <label htmlFor="quantity">How many poem prompts do you want:</label>
+          <input type="number" id="quantity" name="quantity" min="1" onChange={(e) => setAmount(e.target.value)} />
+          <br /><br />
+          { !isPending && <button type="submit" className="submit">Submit</button>}
+          { isPending && <button type="submit" className="submit" disabled>Generating Prompts...</button>}
+        </form>
+        { response && (
+          <div className="response-container">
+            <div className="response">{response}</div>
+          </div>
+        )}
+        { error && (
+          <div className="error-container">
+            <div className="error">{error}</div>
+          </div>
+      )}
+      </div>
+    </div>
+  );
+};
--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -2,47 +2,53 @@ import React, { useState } from 'react';
 import axios from 'axios';
 
 export const Search = () => {
-    const [question, setQuestion] = useState('');
-    const [response, setResponse] = useState('');
-    const [isPending, setIsPending] = useState(false);
-  
-    const handleSubmit = (e) => {
-      e.preventDefault();
-      setIsPending(true);
+	const [question, setQuestion] = useState('');
+	const [response, setResponse] = useState('');
+	const [isPending, setIsPending] = useState(false);
+	const [error, setError] = useState('');
 
-      const headers = {
-        'Content-Type': 'application/json'   
-      };
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		setIsPending(true);
+		setError('');
 
-      const requestBody = {
-        question: question
-      };
+		const headers = {
+			'Content-Type': 'application/json'   
+		};
+		const requestBody = {
+			question: question
+		};
 
-      axios.post('https://poem-assistant-api.onrender.com/search', requestBody, { headers })
-        .then(response => {
-          setResponse(response.data.message);
-          setIsPending(false);
-        })
-        .catch(error => {
-          console.error("there is an error", error)
-        });
-    };
+		axios.post('https://poem-assistant-api.onrender.com/search', requestBody, { headers })
+			.then(response => {
+				setResponse(response.data.message);
+				setIsPending(false);
+			})
+			.catch(error => {
+				console.error("there is an error", error)
+			});
+	};
 
-    return (
-      <div className="form">
-        <h3>General Search</h3>
-        <form className="poem-form" onSubmit={handleSubmit}>
-          <label htmlFor="quantity">What would you like to ask:</label>
-          <input type="text" id="word" name="word" onChange={(e) => setQuestion(e.target.value)}/>
-          <br /><br />
-          { !isPending && <button type="submit" className="submit">Submit</button>}
-          { isPending && <button type="submit" className="submit" disabled>Answering question...</button>}
-        </form>
-        { response && (
-          <div className="response-container">
-            <div className="response">{response}</div>
-          </div>
-        )}
-      </div>
-    );
-  };
+	return (
+		<div className="form">
+			<h3>General Search</h3>
+			<form className="poem-form" onSubmit={handleSubmit}>
+				<label htmlFor="quantity">What would you like to ask:</label>
+				<input type="text" id="word" name="word" onChange={(e) => setQuestion(e.target.value)}/>
+				<br /><br />
+				{ !isPending && <button type="submit" className="submit">Submit</button>}
+				{ isPending && <button type="submit" className="submit" disabled>Answering question...</button>}
+			</form>
+			{ response && (
+				<div className="response-container">
+					<div className="response">{response}</div>
+				</div>
+			)}
+			{ error && (
+        <div className="error-container">
+          <div className="error">{error}</div>
+        </div>
+      )}
+		</div>
+	);
+};
