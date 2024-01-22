17290d71488f89ccf20a20e393060bd2aa329cf6
Ian Ross <ianrossdrums@gmail.com>
2024-01-21 17:37:50 -0700

    Update notes component

--- src/components/Notes/Notes.jsx
+++ src/components/Notes/Notes.jsx
@@ -1,10 +1,44 @@
-// import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 
+export const Notes = (props) => {
+  const [notes, setNotes] = useState('login');
+  const [error, setError] = useState('');
 
-export const Notes = () => {
-    return (
-      <div>
-        <h2>Notes</h2>
-      </div>
-    );
+  useEffect(() => {
+    renderNotes();
+  }, [props]);
+
+  const renderNotes = async (e) => {
+    try {
+      const response = await fetch('http://localhost:8080/notes', {
+      // const response = await fetch('https://poem-assistant-api.onrender.com/notes', {
+        credentials: 'include', 
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        setNotes(data);
+      } else {
+        props.onFormSwitch('login')
+      }
+    } catch (error) {
+      console.error('Error fetching notes:', error);
+    }
+  }
+
+  const handleLogout = async (e) => {
+  }
+
+  return (
+    <div>
+    <h2>Notes</h2>
+    {error && <p>{error}</p>}
+    <ul>
+      {notes.map((note) => (
+        <li key={note.id}>{note.content}</li>
+      ))}
+    </ul>
+    <button onClick={handleLogout}>Logout</button>
+  </div>
+  );
 };
